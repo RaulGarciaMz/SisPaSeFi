a160207874e0b48504f1626a4a8beda70ee78cb8
Raul Garcia <rgarcia@ineel.mx>
2023-02-10 14:01:03 -0600

    fix: cambia la bdd en los contextos hacia la nueva estructura

--- Infrastructure/Data/ProgramaContext.cs
+++ Infrastructure/Data/ProgramaContext.cs
@@ -14,7 +14,7 @@ namespace SqlServerAdapter.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data source = E02626; Initial Catalog = ssf; Integrated Security=True; TrustServerCertificate=True; Trusted_Connection=True; User Id=sa; Password=mi4lia5es_rg@rci@");
+            optionsBuilder.UseSqlServer("Data source = E02626; Initial Catalog = SSF2; Integrated Security=True; TrustServerCertificate=True; Trusted_Connection=True; User Id=sa; Password=mi4lia5es_rg@rci@");
         }
 
         public DbSet<ProgramaPatrullaje> ProgramasPatrullajes { get; set; }
