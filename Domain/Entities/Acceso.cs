ad0496a98778fc4f6b3a303ce4b0a49651b3bd9a
Raul Garcia <rgarcia@ineel.mx>
2023-05-23 13:37:34 -0600

    fix: incluye campo de clave primaria que no existía en BDD

--- Domain/Entities/Acceso.cs
+++ Domain/Entities/Acceso.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities;
@@ -8,6 +7,9 @@ namespace Domain.Entities;
 public partial class Acceso
 {
     [Key]
+    [Column("idaccesos")]
+    public int Id { get; set; }
+
     [Column("fecha", TypeName = "date")]
     public DateTime? Fecha { get; set; }
 
