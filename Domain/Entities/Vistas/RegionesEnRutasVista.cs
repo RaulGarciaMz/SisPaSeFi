98d3ae4506eb13f5a997d457680fd26a75e63152
Raul Garcia <rgarcia@ineel.mx>
2023-05-24 09:22:18 -0600

    fix: modifica de campo a propiedad para serialización de vista de rutas

--- Domain/Entities/Vistas/RegionesEnRutasVista.cs
+++ Domain/Entities/Vistas/RegionesEnRutasVista.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities.Vistas
 {
     [Keyless]
     public class RegionesEnRutasVista
     {
-        public string regionMilitarSDN;
+        public string regionMilitarSDN { get; set; }
     }
 }
